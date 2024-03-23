3d2016dd876c28094dade5d539c79f79152c60ab
Mo Saleh <mohamed.is8890@gmail.com>
2024-03-23 13:57:08 +0200

    Configure API Router

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,26 +3,25 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
   create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
+    .input(
+      z.object({ content: z.string().min(1), authorId: z.string().min(1) }),
+    )
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
       return ctx.db.post.create({
         data: {
-          name: input.name,
+          content: input.content,
+          authorId: input.authorId,
         },
       });
     }),
+
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.db.post.findMany();
+  }),
 
   getLatest: publicProcedure.query(({ ctx }) => {
     return ctx.db.post.findFirst({
