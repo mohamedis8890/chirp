c2757303277449849231a50f524313953a70221e
Mo Saleh <mohamed.is8890@gmail.com>
2024-04-02 00:24:13 +0200

    Post and Profile pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
@@ -71,6 +72,8 @@ type PostWithUser = RouterOutputs["post"]["getAll"][number];
 
 const PostView = (props: PostWithUser) => {
   const { post, author } = props;
+  const handle = `@${author.username}`;
+
   return (
     <div className="flex gap-2 border-b border-slate-400 p-4">
       <div>
@@ -83,8 +86,15 @@ const PostView = (props: PostWithUser) => {
         />
       </div>
 
-      <div className="flex flex-col gap-2 pb-2">
-        <span className="text-slate-400">{`@${author.username} · ${dayjs().to(post.createdAt)}`}</span>
+      <div className="flex flex-col">
+        <div className="flex gap-2 pb-2 text-slate-400">
+          <Link href={`/${handle}`}>
+            <span>{`${handle}`}</span> ·
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span>{`${dayjs().to(post.createdAt)}`}</span>
+          </Link>
+        </div>
         <span className="text-2xl">{post.content}</span>
       </div>
     </div>
